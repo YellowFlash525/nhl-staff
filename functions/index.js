f56f30c26ddcbf7107617f1c465e41299e4cccd5
Kamil Wójcik <kamil.wojcik@cognifide.com>
2018-09-23 01:52:02 +0200

    Server example in Express

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const app = express();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+app.get('/check', (request, response) => {
+  response.send(`${ Date.now()}`);
+})
+
+exports.app = functions.https.onRequest(app);
