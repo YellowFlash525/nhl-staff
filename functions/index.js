2ead5470a0878ddd8ed09dbad80530133fe32db5
Kamil Wójcik <kamil.wojcik@cognifide.com>
2018-12-26 22:26:34 +0100

    Change in project structure, init Vue.js, test api and deploy on firebase

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,15 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const apiPaths = require('./src/apiPaths.json');
 
 const app = express();
 
 app.get('/check', (request, response) => {
-  response.send(`${ Date.now()}`);
+  response.send(`${Date.now()}`);
 })
+
+app.get('/players', (request, response) => {
+  response.send(`${apiPaths.players}`);
+});
 
 exports.app = functions.https.onRequest(app);
